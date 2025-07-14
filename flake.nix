56d6f1330fcd89e270e200b5dfbe985b9756649e
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2025-07-14 17:13:26 +0900

    init: add `proper`

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        proper = pkgs.callPackage ./proper/proper.nix { };
+
+        shell-pkgs = [ (pkgs.texliveFull.withPackages (_: [ proper ])) ];
+        zshCompEnv = pkgs.buildEnv {
+          name = "zsh-comp";
+          paths = shell-pkgs;
+          pathsToLink = [ "/share/zsh" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell rec {
+          packages = shell-pkgs;
+          #ZSH_COMP_FPATH = "${zshCompEnv}/share/zsh/site-functions";
+        };
+      });
+}
---
+++ proper/proper.nix
@@ -0,0 +1,80 @@
+{ stdenvNoCC, lib, fetchurl, texlive, texliveSmall, writeShellScript
+, writableTmpDirAsHomeHook }:
+
+stdenvNoCC.mkDerivation {
+  pname = "proper";
+  version = "1.0.0";
+
+  outputs = [ "tex" ];
+  passthru.tlDeps = with texlive; [ latex ];
+
+  # srcs = [
+  #   (fetchurl {
+  #     url = "http://mirrors.ctan.org/macros/latex/contrib/foiltex/foiltex.dtx";
+  #     hash = "sha256-/2I2xHXpZi0S988uFsGuPV6hhMw8e0U5m/P8myf42R0=";
+  #   })
+  #   (fetchurl {
+  #     url = "http://mirrors.ctan.org/macros/latex/contrib/foiltex/foiltex.ins";
+  #     hash = "sha256-KTm3pkd+Cpu0nSE2WfsNEa56PeXBaNfx/sOO2Vv0kyc=";
+  #   })
+  # ];
+  src = ./.;
+
+  #dontUnpack = true;
+
+  # unpackPhase = ''
+  #   runHook preUnpack
+
+  #   for _src in $srcs; do
+  #     cp "$_src" $(stripHash "$_src")
+  #   done
+
+  #   runHook postUnpack
+  # '';
+
+  nativeBuildInputs = [
+    #(texliveSmall.withPackages (ps: with ps; [ cm-super hypdoc latexmk ]))
+    # multiple-outputs.sh fails if $out is not defined
+    (writeShellScript "force-tex-output.sh" ''
+      out="''${tex-}"
+    '')
+    #writableTmpDirAsHomeHook # Need a writable $HOME for latexmk
+  ];
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  # buildPhase = ''
+  #   runHook preBuild
+
+  #   # Generate the style files
+  #   latex foiltex.ins
+
+  #   # Generate the documentation
+  #   latexmk -pdf foiltex.dtx
+
+  #   runHook postBuild
+  # '';
+
+  installPhase = ''
+    runHook preInstall
+
+    path="$tex/tex/latex/proper"
+    mkdir -p "$path"
+    pwd
+    ls
+    cp proper.sty "$path/"
+    # path="$texdoc/doc/tex/latex/proper"
+    # mkdir -p "$path"
+    # cp *.pdf "$path/"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Configured loader for frequently used packages in LaTeX";
+    license = lib.licenses.wtfpl;
+    #maintainers = with lib.maintainers; [ veprbl ];
+    platforms = lib.platforms.all;
+  };
+}
